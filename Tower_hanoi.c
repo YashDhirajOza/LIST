b545ae07d6cbecb0fd48c1a0963523b72ae216cc
Damm Bro <144452060+YashDhirajOza@users.noreply.github.com>
2024-11-06 10:40:51 +0530

    Update Tower_hanoi.c

--- Tower_hanoi.c
+++ Tower_hanoi.c
@@ -9,6 +9,13 @@ void towerOfHanoi(int n, char from_rod, char to_rod, char aux_rod) {
     towerOfHanoi(n - 1, aux_rod, to_rod, from_rod);
 }
 
+void tower(int n, char A, char B, char C){
+if(n==0){ return;}
+tower(n-1,A,B,C);
+tower(n-1,C,B,A);
+}
+
+
 int main() {
     int N = 3;
 
